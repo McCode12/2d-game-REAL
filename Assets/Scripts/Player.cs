ba973dfa31082fd3010cf943382132f6c698965e
McCode12 <funtimedrexel@gmail.com>
2022-11-05 10:57:23 -0700

    save with player text and player health variable
    saving so i don't forgor about all of the code i coded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,35 @@ public class Player : MonoBehaviour
     [SerializeField] private float vertical;
     [SerializeField] private float movementMultiplier = 0.1f;
 
+    [SerializeField] private Rigidbody2D rb;
+
     [SerializeField] private float diagonalMovementMultiplier = 1.414f;
+
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] public int playerHP = 100;
+
+    [SerializeField] private bool canAttack;
+
+    private void OnCollisionEnter2D(Collision2D collision) {}
+
+
+
+    private IEnumerator gunCooldown() {
+        yield return new WaitForSeconds(1f);
+    }
+
+    
     // Start is called before the first frame update
-    void Start() 
+    void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthText.text = $"Health: {playerHP}";
+        rb.velocity = new Vector2(0, 0);
 
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
@@ -25,6 +45,12 @@ public class Player : MonoBehaviour
             horizontal = horizontal / diagonalMovementMultiplier;
             vertical = vertical / diagonalMovementMultiplier;
         }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        playerHP = playerHP - 10;
     }
 
     void FixedUpdate() {
